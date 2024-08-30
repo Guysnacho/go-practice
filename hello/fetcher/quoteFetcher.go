399c659f566844a98a69f189a1abbe75ee6f090d
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-29 21:42:11 -0500

    Add some notes

--- hello/fetcher/quoteFetcher.go
+++ hello/fetcher/quoteFetcher.go
@@ -1,7 +1,9 @@
+// Defining service package name
 package fetcher
 
 import "rsc.io/quote"
 
+// Functions are usable as long as the package is locatable
 func GetQuote() string {
 	return quote.Go()
 }
--- hello/hello/hello.go
+++ hello/hello/hello.go
@@ -1,8 +1,11 @@
+// Main function isn't called unless this package is named main
 package main
 
 import (
 	"fmt"
 
+	// We've got an alias to the actual fetcher package.
+	// Sorta exporting a different name for it too
 	fetcher "example.com/greetings"
 )
 
