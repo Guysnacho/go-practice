16bf37e66d44a25ac182bbb50acf925af586f918
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-31 03:14:28 -0500

    Park it, let's dive into real shit I think. playing with fake apis is kinda boring

--- 2_SimpleAPI/app/main.go
+++ 2_SimpleAPI/app/main.go
@@ -6,30 +6,33 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-gonic/gin"
 	fetcher "tunjiprod.com/service"
-	util "tunjiprod.com/util"
 )
 
-func fetchSong(w http.ResponseWriter, r *http.Request) {
-	if r == nil {
-		log.Default().Println("No request recieved")
-		http.Error(w, "No request recieved", 401)
+func fetchSong(c *gin.Context) {
+	if c.Request.Method != "GET" {
+		log.Default().Println("Invalid request")
+		c.JSON(http.StatusBadRequest, nil)
 		return
 	}
-	body, err := util.ParseBody(&r.Body)
-	if err != nil {
-		log.Default().Println("Issue parsing")
-		http.Error(w, "Issue parsing", 401)
-		return
-	}
-	fmt.Printf("body: %v\n", body)
 
-	b, _ := fetcher.FetchSong("Tunji")
-	log.Default().Println(b)
-	fmt.Fprintf(w, "Something something, here's lorem :)\n%v", b)
+	// body, err := util.ParseBody(&r.Body)
+	// if err != nil {
+	// 	log.Default().Println("Issue parsing")
+	// 	http.Error(w, "Issue parsing", 401)
+	// 	return
+	// }
+	// fmt.Printf("body: %v\n", body)
+
+	lorem, _ := fetcher.FetchSong("Tunji")
+	log.Default().Println(lorem)
+	fmt.Fprintf(c.Writer, "Something something, here's lorem :)%v", lorem)
 }
 
 func main() {
-	http.HandleFunc("/", fetchSong)
-	http.ListenAndServe(":8080", nil)
+	router := gin.Default()
+	router.GET("/", fetchSong)
+
+	router.Run("localhost:8080")
 }
--- 2_SimpleAPI/service/SongFetcher.go
+++ 2_SimpleAPI/service/SongFetcher.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"io"
 	"log"
 	"net/http"
 )
@@ -9,21 +8,29 @@ import (
 func FetchSong(query string) (string, error) {
 	log.SetFlags(log.Lshortfile)
 	log.SetPrefix("SongFetcher: ")
-	client := &http.Client{}
 
-	req, err := http.NewRequest("get", "https://loripsum.net/api/1/short", nil)
+	res, err := http.Get("https://loripsum.net/api/1/short")
 	if err != nil {
-		log.Default().Print("Issue building request")
+		log.Default().Print("Issue requesting data")
 		return "", err
 	}
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	var data []byte
+
+	log.Default().Println("Response recieved")
+	log.Default().Println(res)
+
+	defer res.Body.Close()
 
 	if res != nil && res.StatusCode == 200 {
-		b, _ := io.ReadAll(res.Body)
-		return string(b), nil
+		_, err := res.Body.Read(data)
+		if err != nil {
+			return "", err
+		} else {
+			log.Default().Println("Data recieved")
+			log.Default().Println(data)
+
+			return string(data), nil
+		}
 	}
 	return "", nil
 }
